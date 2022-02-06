b08d7235d1cae09e974a83acf48afdb29e9d26b6
patton-git <63215900+patton-git@users.noreply.github.com>
2022-02-06 22:08:43 +0900

    Update 06-Add_AD_Users.ps1

--- vSphere ICM/06-Add_AD_Users.ps1
+++ vSphere ICM/06-Add_AD_Users.ps1
@@ -1,21 +1,30 @@
+## 파일을 다운로드하여 실항할 경우, Powershell의 실행 정책 설정을 확인해 주시길 바랍니다.
+## Set-ExecutionPolicy -ExecutionPolicy Undefined -Scope CurrentUser
+
+
+## AD 모듈 설치
 Import-Module ActiveDirectory
 
+## 주요 변수 설정
 $dcpath="DC=vclass,DC=local"
 $ou="Student"
+$groupname="Students"
+$oupath="OU=" + $ou + "," + $dcpath
+
+## OU 만들기
 New-ADOrganizationalUnit -Name $ou -Path $dcpath
 
-
-$oupath="OU=" + $ou + "," + $dcpath
+## 생성된 OU를 삭제하고자 할 경우, 삭제 방지설정을 해제해야 함.
 ## Get-ADOrganizationalUnit -Identity $oupath | Set-ADObject -ProtectedFromAccidentalDeletion:$false -PassThru | Remove-ADOrganizationalUnit -Confirm:$false
 
-$groupname="Students"
+## 생성된 OU내에 보안그룹 생성
 New-ADGroup -Name $groupname -GroupCategory Security -GroupScope Global -DisplayName $groupname -Path $oupath
 
+## AD 사용자를, OU내에 생성 + 그룹에 등록
 $count=0..10
 foreach ($i in $count)
 { 
   $username="S" + "{0:d2}" -f $i
   New-AdUser -Name $username -Path $oupath -Enabled $True -AccountPassword (ConvertTo-SecureString "VMware1!" -AsPlainText -force) -passThru  -PasswordNeverExpires 1
-
   Add-ADGroupMember -Identity $groupname -Members $username
  }
