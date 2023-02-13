ff0e1b92704db3fbbc35be7a395cfc342cbaf2fa
patton-git <63215900+patton-git@users.noreply.github.com>
2023-02-13 14:30:03 +0900

    Update nfs_server.ps1

--- PowerCli/nfs_server.ps1
+++ PowerCli/nfs_server.ps1
@@ -10,16 +10,13 @@ Get-WindowsFeature *nfs*
 Install-WindowsFeature FS-NFS-Service -IncludeAllSubFeature -IncludeManagementTools
 Import-Module NFS
 
-
 New-NfsShare -Name 'nfs01' -Path 'D:\nfs01' -EnableUnmappedAccess $True -Authentication sys
 
+# Grant-NfsSharePermission -Name 'nfs01' -ClientName 'all' -ClientType 'builtin' -Permission 'readwrite' -AllowRootAccess:$true
 
-Grant-NfsSharePermission -Name 'nfs01' -ClientName 'all' -ClientType 'builtin' -Permission 'readwrite' -AllowRootAccess:$true
-Grant-NfsSharePermission -Name 'nfs01' -ClientName 'all' -ClientType 'builtin' -Permission 'readwrite' -AllowRootAccess:$true
-
-# Grant-NfsSharePermission -Name 'nfs01' -ClientName 'sa-esxi-01.vclass.local' -ClientType 'Host' -Permission 'readwrite' -AllowRootAccess:$true
-# Grant-NfsSharePermission -Name 'nfs01' -ClientName 'sa-esxi-02.vclass.local' -ClientType 'Host' -Permission 'readwrite' -AllowRootAccess:$true
-# Grant-NfsSharePermission -Name 'nfs01' -ClientName 'sa-esxi-03.vclass.local' -ClientType 'Host' -Permission 'readwrite' -AllowRootAccess:$true
+Grant-NfsSharePermission -Name 'nfs01' -ClientName 'sa-esxi-01.vclass.local' -ClientType 'Host' -Permission 'readwrite' -AllowRootAccess:$true
+Grant-NfsSharePermission -Name 'nfs01' -ClientName 'sa-esxi-02.vclass.local' -ClientType 'Host' -Permission 'readwrite' -AllowRootAccess:$true
+Grant-NfsSharePermission -Name 'nfs01' -ClientName 'sa-esxi-03.vclass.local' -ClientType 'Host' -Permission 'readwrite' -AllowRootAccess:$true
 
 Get-NfsShare
 Get-NfsSharePermission
