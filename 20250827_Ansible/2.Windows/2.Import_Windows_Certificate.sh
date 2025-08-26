b227e82849620b0351efc15ff539a778f94bbb0b
patton-git <63215900+patton-git@users.noreply.github.com>
2025-08-26 21:55:39 +0900

    Create 2.Import_Windows_Certificate.sh

---
+++ 20250827_Ansible/2.Windows/2.Import_Windows_Certificate.sh
@@ -0,0 +1,13 @@
+## WSL - Ubuntu
+## sudo sed -i '/ad\.vclass\.local/s/^/#/' /etc/hosts
+echo '' | openssl s_client -connect dc:5986
+
+## MMC -> Ctrl+M  -> Certificates (Local Machine)
+## windows mmc - certificates에서 export  -> ad.cer
+## ad.cer 파일을 wsl Ubuntu 내부로 복사
+
+## https://documentation.ubuntu.com/server/how-to/security/install-a-root-ca-certificate-in-the-trust-store/index.html
+sudo openssl x509 -inform der -outform pem -in dc.cer -out dc.crt
+sudo apt-get install -y ca-certificates
+sudo cp ad.crt /usr/local/share/ca-certificates
+sudo update-ca-certificates
