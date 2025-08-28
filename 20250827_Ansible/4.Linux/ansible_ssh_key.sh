37a0abd7587a87d93d2ececa457a5bdabf79bd6f
patton-git <63215900+patton-git@users.noreply.github.com>
2025-08-28 14:28:50 +0900

    Update ansible_ssh_key.sh

--- 20250827_Ansible/4.Linux/ansible_ssh_key.sh
+++ 20250827_Ansible/4.Linux/ansible_ssh_key.sh
@@ -1,8 +1,8 @@
-ssh-keygen -t ed25519 -C "ansible@controller" -f ~/.ssh/ansible_key -N ""
+ssh-keygen -t ed25519 -C "ubuntu@controller" -f ~/.ssh/ansible_key -N ""
 
 ## ansible_ssh_key copy menually
 # ssh-copy-id -i ~/.ssh/ansible_key.pub ansible@k8s-controller3
 # ssh ansible@k8s-controller3 -i ~/.ssh/ansible_key
 
 ## ansible_ssh_key copy using playbook
-ansible-playbook -i inventory.ini ansible_key_deploy.yaml -K -vv
+ansible-playbook -i inventory.yaml ansible_key_deploy.yaml -K -vv
